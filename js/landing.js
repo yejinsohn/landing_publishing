086a2a1b5cb35af1e53c4ed4a095b7ef44081d04
SeokGeunHo <kimc6555@gmail.com>
2024-01-28 03:00:40 +0900

    석근호. 버튼 작업 중간 정리

--- js/landing.js
+++ js/landing.js
@@ -1,57 +1,127 @@
-$(document).ready(function(){
-    $(".menu-box .menu").click(function (){
-        var index = $(this).index();
+$(document).ready(function () {
+    changeMenu(($(".menu-box .menu").index()));
 
-        //Menu Event
-        $(".menu-box .menu").removeClass("click");
-        $(this).addClass("click");
-
-        //Tap area Event
-        $(".form-box .menu-area").hide();
-        $(".form-box .menu-area").eq(index).show();
+    $(".menu-box .menu").click(function () {
+        changeMenu($(this).index()); // 클릭한 메뉴의 index 전달
     });
+
+    checkInputs();
+
+    // text 타입 input과 radio 타입 input이 변화할 때마다 checkInputs 함수 호출
+    for (let index = 1; index <= $(".menu-box .menu").length; index++) {
+        $('#step' + index + ' .input-box, #step' + index + ' .radio, #step' + index + ' .checkbox').on('propertychange change keyup paste input', checkInputs);
+    }
 });
 
-// button 
-function next(idx){
-    $(".form-box .menu-area").hide();
-    $(".form-box .menu-area").eq(idx + 1).show();
-
-    //Menu Event Step 활성화
+// 메뉴를 바꿀때 호출. 다음 버튼에 사용.
+function changeMenu(index) {
+    // 좌측 메뉴 탭 활성화
     $(".menu-box .menu").removeClass("click");
-    $(".menu-box .menu").eq(idx + 1).addClass("click");
+    $(".menu-box .menu").eq(index).addClass("click");
+    
+    // 본문 활성화
+    $(".form-box .menu-area").hide();
+    $(".form-box .menu-area").eq(index).show();
 
-    checkEmpty();
+    getInputs(index + 1);
 }
 
-function prev(idx){
-    $(".form-box .menu-area").hide();
-    $(".form-box .menu-area").eq(idx - 1).show();
+// 해당 menu에 존재하는 input의 종류를 가져옴.
+// 메뉴가 바뀔때 한번만 호출하면 됨. 트리거: 다음으로 넘어가는 버튼 클릭 or 메뉴 div가 변경되어 호출됐을 때
+// 예외 경우: step4는 기본으로 있는 7가지 사항에 + step3에서 체크한 선택사항의 input만 가져와야 함.
+// 예외 경우: step5는 무조건 하나를 체크해야 하지만, 텍스트 작성을 안하고 다른걸 체크해도 넘어가게 해야함.
+// input을 모두 검출하고 활성화 된놈들만 activate 클래스를 붙이고 값 작성되면 빼는거로?
+// 그리고 activate가 있으면 버튼 비활성화, 없으면 활성화
+function getInputs(index) {
+    // 초기화. 모든 input의 activate 클래스 제거.
+    $(':input').each(function() {
+        $(this).removeClass("activate");
+    })
+    
+    // 현재 메뉴에서 활성화 된 input 필드에 activate 클래스 붙이기
+    $('#step' + index + ' :input').each(function() {
+        // 예외 경우: step3, 6은 선택사항 체크 안해도 다음으로 넘어갈 수 있음.
+        // 예외 경우: step3은 추가영역의 텍스트를 작성 안해도 가능하게 해야함.
+        if (index != 3 && index != 6) {
+            $(this).addClass("activate");
+        }
+    })
 
-    //Menu Event
-    $(".menu-box .menu").removeClass("click");
-    $(".menu-box .menu").eq(idx - 1).addClass("click");
+    if ($('#checkbox-2-6').is(':checked')) {
+        $('#step3 .input-box').addClass("activate");
+    }
+
+    // $('#step4 :input').each(function(intdex) {
+    //     console.log($(this).attr('name'));
+    // })
+
+    checkInputs();
 }
 
-function checkEmpty() {
-    const inputclass = $('#step1 .input-box').val();
-    console.log(inputclass);
+// $('input:checkbox[class="checkbox big-checkbox"]:checked') 이 부분에서 클래스명은 정확히 다 써줘야 동작함. 
+// activate가 들어가면 들어간대로 다 써줘야함.
+// 저장해야 할 데이터들
+// step1 : 상호명, 색상조합, 강조이미지
+// step2 : 메인화면 강조부분
+// step3 : 추가영역(직접입력시)
+// step4 : 특징(중복), 메뉴소개, 영수증, 인테리어(유무체크), 후기(중복), 상담신청폼, 가맹절차, 그래프(중복), 유튜브, 인스타, 브랜드소개, 창업비용
+// step5 : 강조폰트(직접입력있음)
+// step6 : 스티키바
+// step7 : 담당자명, 휴대폰번호, 이메일주소
+function checkInputs() {
+    var allTextInputsFilled = true;
+    var oneRadioChecked = false;
+    var oneCheckboxChecked = false;
+
+    // 각 text 타입 input을 확인하며 값이 비어있는지 여부를 확인
+    $('#step' + ($(".menu-box .menu").index() + 1) + ' .input-box').each(function () {
+        if ($(this).val() === '') {
+            allTextInputsFilled = false;
+        }
+    });
+    
+    // radio 타입 input 중 하나만 체크되어 있는지 확인
+    $('#step' + ($(".menu-box .menu").index() + 1) + ' .radio').each(function () {
+        if ($(this).prop('checked')) {
+            oneRadioChecked = true;
+        }
+    });
+    
+    $('#step' + ($(".menu-box .menu").index() + 1) + ' .checkbox').each(function () {
+        if ($(this).prop('checked')) {
+            oneCheckboxChecked = true;
+        }
+    });
+    
+    console.log(allTextInputsFilled);
+    console.log(oneRadioChecked);
+    console.log(oneCheckboxChecked);
+    // 모든 text 타입 input이 채워져 있고, radio 타입 input 중 하나만 체크되어 있으면 버튼 활성화
+    if ((allTextInputsFilled && oneRadioChecked && oneCheckboxChecked)) {
+        $('.next-btn').attr("onclick", "changeMenu(" + ($(".menu-box .menu").index() + 1) + ")");
+        $(".next-btn").removeClass("inactive");
+    } else {
+        $('.next-btn').attr('onclick', '').unbind('click');
+        $(".next-btn").addClass("inactive");
+    }
 }
 
 // Step3 구성 선택1 -> 구성 선택2 테이블 출력
 function showOptionalTableDiv() {
-    if($('#checkbox-2-6').is(':checked')) {
+    if ($('#checkbox-2-6').is(':checked')) {
         $('#step3 .input-box').show();
+        $('#step3 .input-box').addClass("activate");
     } else {
         $('#step3 .input-box').hide();
+        $('#step3 .input-box').removeClass("activate");
     }
     var check_arr = [];
     $('.optional-td').hide();
-    $('input[class="checkbox big-checkbox"]:checked').each(function() {
-        check_arr.push($(this).attr('id'))
+    $('input:checkbox[class="checkbox big-checkbox"]:checked').each(function () {
+        check_arr.push($(this).attr('id'));
     })
-    
-    for(var i = 0; i < check_arr.length; i++) {
+
+    for (var i = 0; i < check_arr.length; i++) {
         $('#td-' + check_arr[i]).show();
     }
 }
@@ -123,11 +193,11 @@ function duplicableCheckbox(childDivType) {
     var checkedbox_array = [];
     $('.' + childDivType + '-div').hide(); // 내용이 들어가있는 div 숨기기.
 
-    $("input[name='" + childDivType + "-checkbox']:checked").each(function() {
+    $("input[name='" + childDivType + "-checkbox']:checked").each(function () {
         checkedbox_array.push($(this).attr('id'))
     })
 
-    for(var i = 0; i < checkedbox_array.length; i++) {
+    for (var i = 0; i < checkedbox_array.length; i++) {
         $('#div-' + checkedbox_array[i]).show();
     }
 }
@@ -136,13 +206,13 @@ function duplicableCheckbox(childDivType) {
 // 사용처: Step4 상담신청폼, Step4 가맹절차
 function singleCheckbox(checkedbox, childDivType) {
     $('.' + childDivType + '-div').hide(); // 내용이 들어가있는 div 숨기기.
-    
-    if($(checkedbox).prop('checked')) { // 체크박스의 체크유무를 확인하여 출력.
-        $("input[type='checkbox'][name='" + childDivType + "-checkbox']").prop('checked',false);
-        $(checkedbox).prop('checked',true);
+
+    if ($(checkedbox).prop('checked')) { // 체크박스의 체크유무를 확인하여 출력.
+        $("input[type='checkbox'][name='" + childDivType + "-checkbox']").prop('checked', false);
+        $(checkedbox).prop('checked', true);
         $('#div-' + $(checkedbox).attr('id')).show();
     } else {
-        $(checkedbox).prop('checked',false);
+        $(checkedbox).prop('checked', false);
     }
 }
 
